13e7b85e4f93529a49479883f2ae94ff157bed22
marcos-aurelio-etec <marcosllima.marcia@gmail.com>
2024-10-25 11:27:35 +0000

    aula legal 25-10

---
+++ src/main/java/application/model/jogo.java
@@ -0,0 +1,31 @@
+package application.model;
+
+import java.util.Set;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "jogo")
+public class jogo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String titulo;
+
+    @ManyToMany
+    @JoinTable(name = "jogos_possuem_plataformas",
+    joinColumns = @JoinColumn(name = "id_jogo"),
+    inverseJoinColumns = @JoinColumn(name = "id_plataforma"))
+    
+    private Set<Plataforma> plataformas;
+}
+
